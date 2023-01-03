f5f788cadbf1764e3cd331967d50a7bdb52f0be4
yash <yash.khandelwal@aurigait.com>
2023-01-03 22:15:50 +0530

    Backend Infra Set-up

---
+++ src/test/java/com/yash/tabletennis/TableTennisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yash.tabletennis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableTennisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
